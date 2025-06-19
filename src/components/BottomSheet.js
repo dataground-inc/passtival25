28f3f8dc3df9fa78fad67856e5ed6368806a8a5a
지훈한 <hoony@jihunhan-ui-MacBookAir.local>
2025-06-19 17:53:30 +0900

    dark mode bottom sheet 적용

--- src/components/BottomSheet.js
+++ src/components/BottomSheet.js
@@ -12,13 +12,13 @@ export const BottomSheet = ({ onClose, onSubmit }) => {
     return (
         <div className="bottom-sheet-overlay" onClick={onClose}>
             <div className="bottom-sheet" onClick={(e) => e.stopPropagation()}>
-                <div><div className='font-20b'>수험번호</div>
+                <div>
+                    <div className='font-20b'>수험번호</div>
                     <input className='font-16r'
                         type="text"
                         value={examNumber}
                         onChange={(e) => setExamNumber(e.target.value)}
                         placeholder="수험 번호를 입력해주세요"
-
                     /></div>
 
                 <div style={{ marginTop: '0px', display: 'flex', gap: '12px' }}>
